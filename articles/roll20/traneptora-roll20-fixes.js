8488f5ca9048999a44f879adbe451f30c446806c
Leo Izen <leo.izen@gmail.com>
2025-04-05 00:35:22 -0400

    roll20: add updateURL

--- articles/roll20/traneptora-roll20-fixes.js
+++ articles/roll20/traneptora-roll20-fixes.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Traneptora's Roll20 Cleanup Script
 // @namespace    https://traneptora.com/
-// @version      2025.04.04.04
+// @version      2025.04.04.05
+// @updateURL    https://raw.githubusercontent.com/Traneptora/roll20-cleanup/refs/heads/main/traneptora-roll20-fixes.js
 // @downloadURL  https://raw.githubusercontent.com/Traneptora/roll20-cleanup/refs/heads/main/traneptora-roll20-fixes.js
 // @description  Traneptora's Roll20 Cleanup Script
 // @author       Traneptora
